342f1fcec0d83130a3082e32b8209f8bd6916a52
dustinmbp <dustin.wehr@gmail.com>
2017-04-15 17:07:14 -0400

    disable some constants that belong in the project-specific file

--- constants.py
+++ constants.py
@@ -1,25 +1,25 @@
 import re
 
 # used by innocent_macros_main.py:
-MACROS_PROJECT_ROOT = "/Users/dustin/Sites/LfP/StructureTogether/scripts/innocent-macros"
-EXPANSION_SCRIPT_NAME = "expand_macros.py"
-DELETION_SCRIPT_NAME = "delete_macros.py"
+# MACROS_PROJECT_ROOT = "/Users/dustin/Sites/LfP/StructureTogether/scripts/innocent-macros"
+# EXPANSION_SCRIPT_NAME = "expand_macros.py"
+# DELETION_SCRIPT_NAME = "delete_macros.py"
 
 ST_ROOT = "/Users/dustin/Sites/LfP/StructureTogether"
 
 TS_MACRO_DEFS_PATH = ST_ROOT + "/src/macros/dev-mode/dwmacros_multiline.ts"
 SRC_WITH_MACRO_OCCURRENCES_TO_PROCESS = ST_ROOT + "/out/structure-together-main.webpack-entry-chunk.js"
 
-WITH_MACRO_OCCURRENCES_TO_PROCESS = [
-    "/out/structure-together-main.webpack-entry-chunk.js",
-    "/out/structure-together-core.webpack-entry-chunk.js",
-    "/out/structure-together-test.webpack-entry-chunk.js",
-    "/out/syncing-data.webpack-entry-chunk.js",
-    "/out/util.webpack-entry-chunk.js",
-    "/out/ifpd-language.webpack-entry-chunk.js"
-]
+# WITH_MACRO_OCCURRENCES_TO_PROCESS = [
+#     "/out/structure-together-main.webpack-entry-chunk.js",
+#     "/out/structure-together-core.webpack-entry-chunk.js",
+#     "/out/structure-together-test.webpack-entry-chunk.js",
+#     "/out/syncing-data.webpack-entry-chunk.js",
+#     "/out/util.webpack-entry-chunk.js",
+#     "/out/ifpd-language.webpack-entry-chunk.js"
+# ]
 
-PATHS_WITH_MACRO_OCCURRENCES_TO_PROCESS = [ST_ROOT + x for x in WITH_MACRO_OCCURRENCES_TO_PROCESS]
+# PATHS_WITH_MACRO_OCCURRENCES_TO_PROCESS = [ST_ROOT + x for x in WITH_MACRO_OCCURRENCES_TO_PROCESS]
 
 USING_EXTERNAL_MODULES = True
 
@@ -28,6 +28,7 @@ DEBUGGING = True
 
 # used by common.py
 DEB_PRINT_ON = True
+# used in expand_macros.py
 VERBOSE_DEFAULT = True
 
 HAS_BEEN_PROCESSED_MARKER__PRODUCTION = "// [MCM] MACROS DELETED"
@@ -35,7 +36,6 @@ HAS_BEEN_PROCESSED_MARKER__DEV = "// [MCM] MACROS EXPANDED"
 INSERT_CONSOLE_LOG_OF_BUILD_TIME = True
 
 NON_WORD_CHAR = "[^a-zA-Z0-9_$.]"
-NON_WORD_CHAR_OR_START_OF_STRING = "(?:^|[^a-zA-Z0-9_$.])"
 
 if USING_EXTERNAL_MODULES:
     RE_FOR_STUFF_BEFORE_MACRO_NAME = "".join([
